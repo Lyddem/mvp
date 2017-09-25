59c55d6642cd6a0b9459d2a78e30df64ee54962a
Lyddem <lydiademi@gmail.com>
2017-09-25 16:14:24 -0700

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,16 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  entry: `${SRC_DIR}/app.js`,
+  entry: `${SRC_DIR}/index.jsx`,
+  devtool: 'source-map',
   output: {
     path: DIST_DIR,
     filename: 'bundle.js'
-  },
+},
+
+// resolve: {
+//   extensions: ['.js','.jsx']
+// },
 
   module: {
     loaders: [
@@ -18,9 +23,9 @@ module.exports = {
      loader: 'babel-loader',
      exclude: /node_modules/,
      query: {
-      presets:['es2015', 'react'] }
+     presets:['es2015', 'react']}
 
      }
     ]
-  },
+  }
 };
