eb1d682283b17c57e28aba5de8c05b9e333bf1a3
Lyddem <lydiademi@gmail.com>
2017-09-26 10:19:55 -0700

    Added onChange

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class Landing extends React.Component {
  constructor(props) {
    super(props);
+
+   var state = {
+    word: ''
+   };
+
+   this.handleChange = this.handleChange.bind(this)
+   this.submitText = this.submitText.bind(this)
  }
+
+handleChange() {
+  alert(this.state.word);
+  console.log(this.state.word);
+}
+
+submitText(phrase){
+  // axios.post('/submitText', this.state.word);
+  // .then(function(res) {
+  //   console.log('client: posted successfully');
+  // });
+};
 
   render () {
     return (
@@ -12,8 +32,8 @@ class Landing extends React.Component {
            <div id = 'header'> Memorize IT </div>
 
             Type in what you'd like to memorize <br /> <br />
-            <input type="text"></input>
-            <input type="submit"></input> <br /><br /><br />
+            <input type="text" onChange={(e)=>{this.setState({word: e.target.value})}}></input>
+            <input type="submit" onClick={this.handleChange}></input> <br /><br /><br />
 
             Test Yourself!
         </div>
