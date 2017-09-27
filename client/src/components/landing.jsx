f9d623d98286a6b4487afe36549b7a5297f3988d
Lyddem <lydiademi@gmail.com>
2017-09-26 19:26:49 -0700

    Added get request

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -6,13 +6,14 @@ class Landing extends React.Component {
    super(props);
 
    var state = {
-    word: ''
+    word: '',
+    practice: '',
+    lastWord: ''
    };
 
    this.submitText = this.submitText.bind(this);
+   this.checkAnswer = this.checkAnswer.bind(this);
  }
-
-
 
 submitText() {
   axios.post('/submitText', {data: this.state.word})
@@ -24,6 +25,30 @@ submitText() {
   })
 }
 
+checkAnswer(){
+  var practice = this.state.practice; //word to compare with last word from database!
+  console.log(practice, '<--PRACTICE')
+  axios.get('/getLastWord')
+  .then((response) => {
+    console.log('response FROM CHECKANSWER', response.data[0].word)
+    this.setState({lastWord: response.data[0].word})
+
+    if(response.data[0].word === this.state.practice) {
+      alert("YEA!");
+    } else {
+      alert("Nope, try again!");
+    }
+
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+
+  // console.log('last word')
+  // if(this.state.practice === this.state.lastWord) {
+  //   console.log('TRUE');
+  // }
+}
   render () {
     return (
       <div>
@@ -34,10 +59,10 @@ submitText() {
             <input type="text" onChange={(e)=>{this.setState({word: e.target.value})}}></input>
             <input type="submit" onClick={this.submitText}></input> <br /><br /><br />
 
-            Test Yourself!
+            Test Yourself!  <br /> <br />
+             <input type="text" placeholder="re-write your text here" onChange={(e)=>{this.setState({practice:e.target.value})}}></input>
+            <input type="submit" onClick={this.checkAnswer}></input> <br /><br /><br />
         </div>
-
-
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -19,17 +19,29 @@ const connection = mysql.createConnection({
 app.post('/submitText', function(req, res) {
 
   var query = `INSERT INTO words (word) VALUES ("${req.body.data}")`
-  connection.query(query, function(result,err){
+  connection.query(query, function(err, result) {
     if(err){
-      console.log('err', err);
+      console.log(err)
     } else {
-      res.send(req.data);
+      res.end(req.body.data);
     }
   })
 
   console.log('REQ BODY', req.body.data);
   res.send(req.body.data) //send back the request
 });
+
+app.get('/getLastWord', function(req, res){
+  var query = `SELECT word FROM words WHERE id = (SELECT MAX(id) from words)`;
+  // var query = `SELECT * FROM  words`;
+  connection.query(query, function(err, result) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  })
+})
 
 app.get('/', function(req, res) {
   // res.send('server');
@@ -38,4 +50,3 @@ app.get('/', function(req, res) {
 app.listen(PORT, function(req, res) {
   console.log('Listening on Port ', PORT);
 });
-
